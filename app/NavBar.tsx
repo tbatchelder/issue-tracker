cc59bd25dad91f164ed0790d9a6ab063f09b7955
tbatchelder <pegacorn@optonline.net>
2025-02-18 19:56:44 -0500

    Build of the nav bar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,17 +1,27 @@
 import Link from "next/link";
 import React from "react";
+import { GiUnicorn } from "react-icons/gi";
 
 const NavBar = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
   return (
-    <nav className="flex space-x-6">
-      <Link href="/">Logo</Link>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <GiUnicorn />
+      </Link>
       <ul className="flex space-x-6">
-        <li>
-          <Link href="/">Dashboard</Link>
-        </li>
-        <li>
-          <Link href="/issues">Issues</Link>
-        </li>
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
