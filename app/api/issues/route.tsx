3cd5b7474350746214ca98d863bfe53dd5d92f36
tbatchelder <pegacorn@optonline.net>
2025-02-22 23:20:19 -0500

    Build of prisma client

---
+++ app/api/issues/route.tsx
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+
+const createIssueSchema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+
+  // Check that the validation is good
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 }); // 400 = Bad Request
+
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description },
+  });
+
+  return NextResponse.json(newIssue, { status: 201 }); // 201 = object created
+}
