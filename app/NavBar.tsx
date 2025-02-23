c9e57782cfc92766b08880eb1c8d7eca92be8955
tbatchelder <pegacorn@optonline.net>
2025-02-22 20:30:08 -0500

    Styled the link using classname

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import React from "react";
 import { usePathname } from "next/navigation";
 import { GiUnicorn } from "react-icons/gi";
+import classNames from "classnames";
 
 const NavBar = () => {
   const currentPath = usePathname();
@@ -21,11 +22,20 @@ const NavBar = () => {
       </Link>
       <ul className="flex space-x-6">
         {links.map((link) => (
+          // <Link
+          //   key={link.href}
+          //   className={`${
+          //     link.href === currentPath ? "text-zinc-900" : "text-zinc-500"
+          //   } hover:text-zinc-800 transition-colors`}
+          //   href={link.href}
+          // >
           <Link
             key={link.href}
-            className={`${
-              link.href === currentPath ? "text-zinc-900" : "text-zinc-500"
-            } hover:text-zinc-800 transition-colors`}
+            className={classNames({
+              "text-zinc-900": link.href === currentPath,
+              "text-zinc-500": link.href !== currentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
             href={link.href}
           >
             {link.label}
